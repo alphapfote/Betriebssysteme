128011ebc619975e8cb7cf1735864c9a9e70378c
rual1017 <rual1017@IWI-lkit3-03.HS-Karlsruhe.DE>
2017-11-22 16:21:11 +0100

    Added Inode and Superblock with comments

--- src/mkfs.myfs.cpp
+++ src/mkfs.myfs.cpp
@@ -9,10 +9,48 @@
 #include "myfs.h"
 #include "blockdevice.h"
 #include "macros.h"
+#include <string>
+#include <iostream>
+using namespace std;
+
+// Wird als Struktur gebaut, da Inodes mehrfach vorhanden sind.
+struct Inode {
+	string filename; // Dateiname
+	unsigned long filesize; // Deteigröße
+	unsigned int uid_t; // User-ID
+	unsigned int gid_t; // Group-ID
+	unsigned short mode_t; // zugriffsberechtigungen (mode)
+	unsigned long atime; // Acces Time (letzter Zugriff)
+	unsigned long mtime; // Modification Time (letzte Änderung)
+	unsigned long ctime; // Status Change Time (letzte Statusänderung)
+	unsigned int* pointerFirstDatablock; // Zeiger	auf	ersten	Datenblock
+};
+
+// Superblock
+unsigned long sizeOfFileSystem;
+unsigned long sizeofInodeList;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 int main(int argc, char *argv[]) {
 
-    // TODO: Implement file system generation & copying of files here
-    
-    return 0;
+	// TODO: Implement file system generation & copying of files here
+
+	return 0;
 }
