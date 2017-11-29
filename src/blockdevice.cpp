a38222ee6ce23a0c174dd2f2b07d0daf35ebfd5d
rual1017 <rual1017@IWI-lkit3-02.HS-Karlsruhe.DE>
2017-11-29 16:15:30 +0100

    Changed Inode Structs and FAT

--- src/blockdevice.cpp
+++ src/blockdevice.cpp
@@ -19,83 +19,78 @@
 #include "blockdevice.h"
 
 BlockDevice::BlockDevice(u_int32_t blockSize) {
-    assert(blockSize % 512 == 0);
-    this->blockSize= blockSize;
+	assert(blockSize % 512 == 0);
+	this->blockSize = blockSize;
 }
 
 void BlockDevice::resize(u_int32_t blockSize) {
-    assert(blockSize % 512 == 0);
-    this->blockSize= blockSize;
+	assert(blockSize % 512 == 0);
+	this->blockSize = blockSize;
 }
 
 int BlockDevice::create(const char *path) {
-    
-    // TODO: handle block devices
-    
-    // Open Container file
-    contFile = ::open(path, O_EXCL | O_RDWR | O_CREAT, 0666);
-    if (contFile < 0) {
-        if (errno == EEXIST)
-            error("container file already exists");
-        else
-            error("unable to create container file");
-    }
-    
-    // read file stats
-    struct stat st;
-    stat(path, &st);
-    
-    // get file size
-    off_t size = st.st_size;
-    if(size > INT32_MAX)
-        error("file to large");
-    this->size= (uint32_t) size;
-    
-    return 0;
+
+	// Open Container file
+	contFile = ::open(path, O_EXCL | O_RDWR | O_CREAT, 0666);
+	if (contFile < 0) {
+		if (errno == EEXIST)
+			error("container file already exists");
+		else
+			error("unable to create container file");
+	}
+
+	// read file stats
+	struct stat st;
+	stat(path, &st);
+
+	// get file size
+	off_t size = st.st_size;
+	if (size > INT32_MAX)
+		error("file to large");
+	this->size = (uint32_t) size;
+
+	return 0;
 }
 
 int BlockDevice::open(const char *path) {
-    
-    // TODO: handle block devices
-    
-    // Open Container file
-    contFile = ::open(path, O_EXCL | O_RDWR);
-    if (contFile < 0) {
-        if (errno == ENOENT)
-            error("container file does not exists");
-        else
-            error("unknown error");
-    }
-    
-    // read file stats
-    struct stat st;
-    stat(path, &st);
-    
-    // get file size
-    if(st.st_size > INT32_MAX)
-        error("file to large");
-    this->size= (uint32_t) st.st_size;
-    
-    return 0;
+
+	// Open Container file
+	contFile = ::open(path, O_EXCL | O_RDWR);
+	if (contFile < 0) {
+		if (errno == ENOENT)
+			error("container file does not exists");
+		else
+			error("unknown error");
+	}
+
+	// read file stats
+	struct stat st;
+	stat(path, &st);
+
+	// get file size
+	if (st.st_size > INT32_MAX)
+		error("file to large");
+	this->size = (uint32_t) st.st_size;
+
+	return 0;
 }
 
-
 int BlockDevice::close() {
-    
-    ::close(this->contFile);
-    
-    return 0;
+
+	::close(this->contFile);
+
+	return 0;
 }
 
 int BlockDevice::read(u_int32_t blockNo, char *buffer) {
-    seekto(this->contFile, blockNo);
-    readbuf(this->contFile, buffer, this->blockSize);
-    return 0;
+	seekto(this->contFile, blockNo);
+	readbuf(this->contFile, buffer, this->blockSize);
+	return 0;
 }
 
-int BlockDevice::write(u_int32_t blockNo, char *buffer) {
+int BlockDevice::write(u_int32_t blockNo, char *buffer) { //Blocknummer, Größe des Blocks 512 Byte lang, Wichtig: richtig allokieren
 	seekto(this->contFile, blockNo);
 	writebuf(this->contFile, buffer, this->blockSize);
-    return 0;
+	return 0;
 }
 
--- src/mkfs.myfs.cpp
+++ src/mkfs.myfs.cpp
@@ -11,9 +11,15 @@
 #include "macros.h"
 #include <string>
 #include <iostream>
+#include <fstream>
+#include <sys/stat.h>
 using namespace std;
 
-
+#define NAME_LENGTH 255 			// Maximale Länge eines Dateinamens in Zeichen
+#define BLOCK_SIZE 512 				// Logische Blockgröße
+#define NUM_INODES 64 				// Anzahl Inodes
+#define NUM_MAX_FILES NUM_INODES 	// Maximale Anzahl Dateien
+#define AMOUNT_BLOCKS 500			// Anzahl der Blöcke
 
 // Wird als Struktur gebaut, da Inodes mehrfach vorhanden sind.
 struct Inode {
@@ -22,45 +28,79 @@ struct Inode {
 	unsigned int uid_t; // User-ID
 	unsigned int gid_t; // Group-ID
 	unsigned short mode_t; // zugriffsberechtigungen (mode)
-	unsigned long atime; // Acces Time (letzter Zugriff)
+	unsigned long atime; // Access Time (letzter Zugriff)
 	unsigned long mtime; // Modification Time (letzte Änderung)
 	unsigned long ctime; // Status Change Time (letzte Statusänderung)
-	unsigned int* pointerFAT; // Zeiger	auf	ersten Eintrag in der FAT für die Datei
+	unsigned int* pointerToFirstFileBlock // Zeiger auf ersten Eintrag in der FAT für die Datei
 };
+struct Inode inodeList[];
 
 // Superblock
 unsigned long sizeOfFileSystem;
 unsigned long sizeofInodeList;
 
 // FAT
-// TODO Inode verweist auf FAT, FAT verweist auf Datei, FAT ist eine einfach verkette Liste von Pointern
-list <unsigned int*> pointerToFileInDirectory;
-
-
+unsigned int* FAT[];
 
 // Root-Verzeichnis
 unsigned char* inodeID; // ID's der Inodes
 
+/**
+ *  Die Methode schreibt eine Datei in den Container
+ *
+ *  @param data die zu schreibende Datei
+ *  @param containerPath Name der zu schreibenden Datei
+ */
+void writeFileInContainer(char* data, char* containerPath) {
+	BlockDevice bd = BlockDevice();
+	bd.create(containerPath);
+
+//	bd.write(1, data);
+	bd.write(1, "HelloWorld");
+
+}
 
 
+/**
+ * Die Methode füllt die Inodes für das Schreiben einer Datei
+ * @param metadata, die Metadaten die für das schreiben der Inode gebraucht werden
+ * @param fileName, Name der Datei
+ */
+void writeInodeData(char* file) {
+	// Erstellen einer leeren Struktur für die Metadaten
+	struct stat metadata;
+	// Füllen der Struktur mit den durch stat ausgelesenen Daten
+	stat(file, &metadata);
+
+	struct Inode inodeName = new Inode;
+	inodeName.filename = file;
+	inodeName.filesize = metadata.st_size;
+	inodeName.uid_t = metadata.st_uid;
+	inodeName.gid_t = metadata.st_gid;
+	inodeName.mode_t = S_IFREG | 0444; // In der Aufgabenstellung gegeben
+	inodeName.atime = metadata.st_atime;
+	inodeName.mtime = metadata.st_mtime;
+	inodeName.ctime = metadata.st_ctime;
+	// TODO Pointer pointer = malloc(filesize)
 
 
+	inodeList[sizeof(inodeList)] = inodeName;
+}
 
 
-
-
-
-
-
-
-
-
-
-
-
+// argv = argument value
+// generische form, mit der ein c++ programm aufgerufen wird
+// erste sagt, wie viele elemente es in argv sind
+// argv[0] ist immer der (Container)Dateiname, besser gesagt Programmname -> default vom betriebssystem
+// argv[1] ist der Name des Containers
 int main(int argc, char *argv[]) {
 
+
 	// TODO: Implement file system generation & copying of files here
+
+	char* containerPath = *argv[1];
+	writeInodeData(argv[2]);
+	writeFileInContainer(argv[2], containerPath);
 
 	return 0;
 }
