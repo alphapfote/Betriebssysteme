fbc6d00d73f70198b219ae640358aa06e02b50b0
Grymare <hendrik.diemer@gmail.com>
2017-12-15 18:59:21 +0100

    Put write/read device in header

--- src/mkfs.myfs.cpp
+++ src/mkfs.myfs.cpp
@@ -10,6 +10,7 @@
 #include "myfs.h"
 #include "blockdevice.h"
 #include "macros.h"
+#include "mkfs.myfs.h"
 #include <string>
 #include <iostream>
 #include <fstream>
@@ -58,48 +59,6 @@ unsigned int FAT[AMOUNT_BLOCKS]; //Größe: Anzahl der Blöcke
 unsigned short inodeID[64]; // ID's der Inodes
 
 
-// Funktion zum Lesen vom Blockdevice, nimmt normale Datentypen und Structs entgegen
-template<typename T> void read_device(BlockDevice* device, u_int32_t block,
-		T* data) {
-	static char buffer[BLOCK_SIZE];
-	device->read(block, buffer);
-	*data = *reinterpret_cast<T*>(buffer);
-}
-
-// Funktion zum Lesen vom  Blockdevice, nimmt Arrays entgegen
-template<std::size_t N, typename T> void read_device(BlockDevice* device,
-		u_int32_t block, T (&data)[N]) {
-	static char buffer[BLOCK_SIZE];
-	device->read(block, buffer);
-	memcpy(&data, buffer, sizeof(T[N])); //geht als schleife durch den array
-}
-
-// data (T) muss ein Array oder Struct sein, kein Pointer
-// Funktion zum Schreiben auf das Blockdevice, nimmt normale Datentypen und Structs entgegen
-template<typename T> void write_device(BlockDevice* device, u_int32_t block,
-		const T* data) {
-	static_assert(sizeof(T) <= BLOCK_SIZE, "T must not be bigger than the block size");
-
-	static char buffer[BLOCK_SIZE];
-
-	memset(buffer, 0, BLOCK_SIZE); // Die 512 Byte des Puffers werden mit 0en belegt, damit er ausgefüllt ist.
-	memcpy(buffer, data, sizeof(T)); // So viel Byte wie nötig werden mit dem Inhalt von Data überschrieben, der Rest bleibt 0
-
-	device->write(block, buffer);
-}
-
-// Funktion zum Schreiben auf das Blockdevice, nimmt Arrays entgegen
-template<std::size_t N, typename T> void write_device(BlockDevice* device,
-		u_int32_t block, const T (&data)[N]) {
-	static_assert(sizeof(T[N]) <= BLOCK_SIZE, "T must not be bigger than the block size");
-
-	static char buffer[BLOCK_SIZE];
-
-	memset(buffer, 0, BLOCK_SIZE);
-	memcpy(buffer, data, sizeof(T[N]));
-
-	device->write(block, buffer);
-}
 
 
 /**
